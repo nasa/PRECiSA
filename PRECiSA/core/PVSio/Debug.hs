6c983e6d5e88f1e978331b550bf62122ba464b81
Marco A. Feliu <marco.feliu@nasa.gov>
2026-07-21 16:01:44 -0400

    core: Add PVSio backend debugging support

---
+++ PRECiSA/core/PVSio/Debug.hs
@@ -0,0 +1,34 @@
+-- Notices:
+--
+-- Copyright 2025 United States Government as represented by the Administrator of the National Aeronautics and Space Administration. All Rights Reserved.
+
+-- Disclaimers
+-- No Warranty: THE SUBJECT SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY OF ANY KIND, EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO SPECIFICATIONS, ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR FREEDOM FROM INFRINGEMENT, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL BE ERROR FREE, OR ANY WARRANTY THAT DOCUMENTATION, IF PROVIDED, WILL CONFORM TO THE SUBJECT SOFTWARE. THIS AGREEMENT DOES NOT, IN ANY MANNER, CONSTITUTE AN ENDORSEMENT BY GOVERNMENT AGENCY OR ANY PRIOR RECIPIENT OF ANY RESULTS, RESULTING DESIGNS, HARDWARE, SOFTWARE PRODUCTS OR ANY OTHER APPLICATIONS RESULTING FROM USE OF THE SUBJECT SOFTWARE.  FURTHER, GOVERNMENT AGENCY DISCLAIMS ALL WARRANTIES AND LIABILITIES REGARDING THIRD-PARTY SOFTWARE, IF PRESENT IN THE ORIGINAL SOFTWARE, AND DISTRIBUTES IT "AS IS."
+
+-- Waiver and Indemnity:  RECIPIENT AGREES TO WAIVE ANY AND ALL CLAIMS AGAINST THE UNITED STATES GOVERNMENT, ITS CONTRACTORS AND SUBCONTRACTORS, AS WELL AS ANY PRIOR RECIPIENT.  IF RECIPIENT'S USE OF THE SUBJECT SOFTWARE RESULTS IN ANY LIABILITIES, DEMANDS, DAMAGES, EXPENSES OR LOSSES ARISING FROM SUCH USE, INCLUDING ANY DAMAGES FROM PRODUCTS BASED ON, OR RESULTING FROM, RECIPIENT'S USE OF THE SUBJECT SOFTWARE, RECIPIENT SHALL INDEMNIFY AND HOLD HARMLESS THE UNITED STATES GOVERNMENT, ITS CONTRACTORS AND SUBCONTRACTORS, AS WELL AS ANY PRIOR RECIPIENT, TO THE EXTENT PERMITTED BY LAW.  RECIPIENT'S SOLE REMEDY FOR ANY SUCH MATTER SHALL BE THE IMMEDIATE, UNILATERAL TERMINATION OF THIS AGREEMENT.
+
+
+module PVSio.Debug
+  ( withSystemTempDirectory
+  ) where
+
+import System.FilePath
+import System.Directory (getTemporaryDirectory, createDirectoryIfMissing)
+import Data.Time.Clock (getCurrentTime)
+import Data.Time.Clock.POSIX (utcTimeToPOSIXSeconds)
+
+-- | Debug version of withSystemTempDirectory that doesn't clean up
+-- Prints the temp directory path and leaves files for inspection
+withSystemTempDirectory :: String -> (FilePath -> IO a) -> IO a
+withSystemTempDirectory template action = do
+  systemTempDir <- getTemporaryDirectory
+  -- Create a unique directory name using timestamp
+  timestamp <- getCurrentTime
+  let timeStr = show (floor (utcTimeToPOSIXSeconds timestamp) :: Integer)
+  let debugDir = systemTempDir </> template ++ timeStr
+  createDirectoryIfMissing True debugDir
+  putStrLn $ "\n=== DEBUG: PVSio temporary directory ==="
+  putStrLn $ "Directory: " ++ debugDir
+  putStrLn $ "Files will NOT be deleted automatically for inspection"
+  putStrLn $ "========================================\n"
+  action debugDir
